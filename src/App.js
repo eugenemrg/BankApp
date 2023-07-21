e0fe1350799b2a10fad77f59365df8ee8214dc62
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-21 20:17:13 +0300

    Created and added main components for app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from 'react'
+import Header from './components/Header';
+import Form from './components/Form';
+import Transactions from './components/Transactions';
+
 function App() {
   return (
     <div className="App">
-      Initial bank app
+      <Header />
+      <Form />
+      <Transactions />
     </div>
   );
 }
