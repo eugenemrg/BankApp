0059155984af9405df75b2a822e39f701fef1695
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-22 00:04:08 +0300

    Updated TransactionSearch component, added a header section, added id and clss attributes, updated content in component

--- src/components/TransactionSearch.js
+++ src/components/TransactionSearch.js
@@ -3,9 +3,13 @@ import React from 'react'
 function TransactionSearch() {
   return (
     <div className='search-container'>
-        <form>
-            <input type='text' placeholder='Enter text to search' required />
-            <button>search</button>
+        <div className='search-title-container'>
+            <p className='search-title'>Transaction Search</p>
+            <i className='search-title-logo'></i>
+        </div>
+        <form id='search-form'>
+            <input type='text' placeholder='Enter text to search' required id='search-form-input' />
+            <button id='search-form-submit'>search</button>
         </form>
     </div>
   )
