55951ab1ec456904e0f7230a24fe8bef15b3bfaa
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-21 22:21:32 +0300

    Updated Header component, added header text and navigation section,navigation children to be added using props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div>
+            <h1>Bank of Union App</h1>
+            <nav>
+            </nav>
+        </div>
+    )
 }
 
 export default Header
