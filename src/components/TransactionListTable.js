a6d750f0e4d238a61bda7f86d2e86b4e1865e9bd
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-21 21:33:03 +0300

    Added TransactionListTableBody component as parent to TransactionListTableRow components to match React guidelines requiring enclosing <tbody> tags for all rows in a table body

--- src/components/TransactionListTable.js
+++ src/components/TransactionListTable.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import TransactionListTableHeader from './TransactionListTableHeader'
+import TransactionListTableBody from './TransactionListTableBody'
 
 function TransactionListTable() {
   return (
     <table>
       <TransactionListTableHeader />
+      <TransactionListTableBody />
     </table>
   )
 }
---
+++ src/components/TransactionListTableBody.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TransactionListTableBody() {
+  return (
+    <tbody>
+        TransactionListTableBody
+    </tbody>
+  )
+}
+
+export default TransactionListTableBodygit commit -m 'Added TransactionListTableBody component as parent to TransactionListTableRow components to match React guidelines requiring enclosing <tbody> elements for each row in a table body'
