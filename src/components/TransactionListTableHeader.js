c14f6a28e63cf74e25b2f8b8e2fc5dcf9a309765
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-21 21:24:48 +0300

    Updated TransactionListTableHeader, added <thead> as parent to match React guidelines

--- src/components/TransactionListTableHeader.js
+++ src/components/TransactionListTableHeader.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 function TransactionListTableHeader() {
-  return (
-    <tr>
-        <th>Date</th>
-        <th>Description</th>
-        <th>Category</th>
-        <th>Amount</th>
-        <th></th>
-    </tr>
-  )
+    return (
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Description</th>
+                <th>Category</th>
+                <th>Amount</th>
+                <th></th>
+            </tr>
+        </thead>
+    )
 }
 
 export default TransactionListTableHeader
