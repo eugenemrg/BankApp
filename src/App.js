2c4a9edef96873b7ba3f526f76cf35db8f9ca8a8
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-22 11:36:00 +0300

    Updated App component, added a useEffect to fetch transactions data, saved the data to a state variable named transactionsData

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './components/Header';
 import Form from './components/Form';
 import Transactions from './components/Transactions';
 import headerNavigationItems from './data/headers';
 
 function App() {
+
+  const [transactionsData, setTransactionsData] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3007/transactions')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setTransactionsData(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
   return (
     <div className="App">
       <Header navItems={headerNavigationItems} />
