97789e0b4084e07a5b58fde56c7d3a6c1744a685
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-22 10:49:28 +0300

    Updated Form Component, added and updated attributes for elements in the Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -21,17 +21,21 @@ function Form() {
     return (
         <div id='form'>
             <div className='transaction-form-title-container' onClick={handleFormToggle}>
-                <p className='transaction-form-title'>Add a transaction</p>
-                <i class={icon}></i>
+                <p className='transaction-form-title'>Add New transaction</p>
+                <i className={icon}></i>
             </div>
             <form id='transaction-form' className={hideStatus}>
-                <input type='date' name='date' id='date' className='form-item'/>
-                <textarea rows='4' placeholder='Add a description for the transaction' name='description' className='form-item'/>
-                <select name='category' className='form-item'>
-                    <option value='Food'>Food</option>
+                <label htmlFor='date'>Transaction Date</label>
+                <input type='date' name='date' id='date' className='form-item' required/>
+                <label htmlFor='description'>Transaction Description</label>
+                <textarea rows='4' placeholder='Add a description for the transaction' name='description' className='form-item' required/>
+                <label htmlFor='category'>Transaction Category</label>
+                <select name='category' className='form-item' required>
+                    <option value='none'>Category</option>
                 </select>
-                <input type='number' name='amount' placeholder='0' className='form-item'/>
-                <input type='submit' value='Save' className='form-item'/>
+                <label htmlFor='amount'>Transaction Amount</label>
+                <input type='number' name='amount' placeholder='Enter amount' className='form-item' required/>
+                <input type='submit' value='Save Transaction' className='form-item'/>
             </form>
         </div>
     )
