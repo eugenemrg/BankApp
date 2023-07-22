f1d658911c71212ec7fd19d3726fe0b40f7b26de
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-22 09:01:33 +0300

    Updated App component to import header navigation items array data and passed data as prop to Header component, updated Header component to implement display of navigation items

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Header from './components/Header';
 import Form from './components/Form';
 import Transactions from './components/Transactions';
+import headerNavigationItems from './data/headers';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header navItems={headerNavigationItems} />
       <Form />
       <Transactions />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-function Header() {
+function Header({navItems}) {
+
+    const navItemsArray = navItems.map(item => {
+        return <a key={item.href} href={item.href} className='nav-item'>{item.header}</a>
+    })
+
     return (
         <div id='header'>
             <h1>Bank App</h1>
             <nav>
+                {navItemsArray}
             </nav>
         </div>
     )
