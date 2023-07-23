3a1ce4b8ef45189372f64290838af19bd5954843
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-23 20:23:15 +0300

    Updated Form component, added state variables for input data, updated form input elements value and change listeners to use the state variables, handled submit action to return formData to App component through handle(addNewTransaction) callback prop from the App component when form is submitted

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Form({categories}) {
+function Form({categories, addNewTransaction}) {
 
     const [isVisible, setVisible] = useState(false)
     const [hideStatus, setHideStatus] = useState('hide')
     const [icon, setIcon] = useState('fa-solid fa-chevron-down fa-rotate-270')
+    const [formData, setFormData] = useState({
+        date: new Date(new Date()).toISOString().split('T')[0],
+        description: '',
+        category: '',
+        amount: ''
+    })
 
     const options = categories.map( (category) => {
         return <option key={category} value={category}>{category}</option>
@@ -22,24 +28,36 @@ function Form({categories}) {
         }
     }
 
+    function handleFormDataUpdate(e) {
+        setFormData({
+            ...formData,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        addNewTransaction(formData)
+    }
+
     return (
         <div id='form'>
             <div className='transaction-form-title-container' onClick={handleFormToggle}>
                 <p className='transaction-form-title'>Add New transaction</p>
                 <i className={icon}></i>
             </div>
-            <form id='transaction-form' className={hideStatus}>
+            <form id='transaction-form' className={hideStatus} onSubmit={handleSubmit}>
                 <label htmlFor='date'>Transaction Date</label>
-                <input type='date' name='date' id='date' className='form-item' required/>
+                <input type='date' name='date' id='date' className='form-item' value={formData.date} onChange={handleFormDataUpdate} required/>
                 <label htmlFor='description'>Transaction Description</label>
-                <textarea rows='4' placeholder='Add a description for the transaction' name='description' className='form-item' required/>
+                <textarea rows='4' placeholder='Add a description for the transaction' name='description' className='form-item' value={formData.description} onChange={handleFormDataUpdate} required/>
                 <label htmlFor='category'>Transaction Category</label>
-                <select name='category' className='form-item' required>
-                    <option value='none'>Category</option>
+                <select name='category' className='form-item' value={formData.category}  onChange={handleFormDataUpdate} required>
+                    <option value=''>Category</option>
                     {options}
                 </select>
                 <label htmlFor='amount'>Transaction Amount</label>
-                <input type='number' name='amount' placeholder='Enter amount' className='form-item' required/>
+                <input type='number' name='amount' placeholder='Enter amount' className='form-item' value={formData.amount} onChange={handleFormDataUpdate} required/>
                 <input type='submit' value='Save Transaction' className='form-item'/>
             </form>
         </div>
