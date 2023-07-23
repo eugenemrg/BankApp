7dd261bf97125ad415fbfa0876486377b9e567a0
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-23 11:52:42 +0300

    Updated TransactionSearch component, updated component stracture and content, received handleSearch, showSearchLoadingIcon props from App component, used showSearchLoadingIcon state variable to decide when loading icon or search icon shows, create state variable for search input

--- src/components/TransactionSearch.js
+++ src/components/TransactionSearch.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function TransactionSearch() {
+function TransactionSearch({ handleSearch, showSearchLoadingIcon }) {
+
+  const [query, setQuery] = useState('')
+
   return (
     <div className='search-container' id='search'>
-        <p id='search-title'>Transaction History</p>
-        <form id='search-form'>
-            <input type='text' placeholder='Enter text to search' required id='search-form-input' />
-            <button id='search-form-submit'>search</button>
-        </form>
+      <p id='search-title'>Transaction History</p>
+      <form id='search-form'>
+        <label htmlFor='search'>Search for transactions</label>
+        <div id='search-input-container'>
+          <input id='search-form-input' name='search' type='text' placeholder='Enter text to search' value={query} onChange={(e) => setQuery(e.target.value)} />
+          <button id='search-form-submit'>{showSearchLoadingIcon ? <i class="fa-solid fa-circle-notch fa-spin"></i> : <i class="fa-solid fa-magnifying-glass"></i>}</button>
+        </div>
+      </form>
     </div>
   )
 }
